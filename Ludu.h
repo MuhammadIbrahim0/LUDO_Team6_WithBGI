dbadbb101ca843da80b42efdb73e256b5edbd9ec
mmuneeburahman <mmuneeburahman@gmail.com>
2020-05-03 12:27:20 +0500

    Added Ludu Class.
    1. Removed Comments from the main.cxx
    2. Made only Constructor and Destructor

---
+++ Ludu.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+class Ludu
+{
+public:
+	Ludu() {
+		cout << "ludu Contructed" << endl;
+	}
+	~Ludu() {
+		cout << "ludu Destructed" << endl;
+	}
+};
+
