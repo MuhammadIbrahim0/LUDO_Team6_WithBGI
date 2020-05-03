e70c1b4f06031c8d4d312b7a2ba4cbd040fd5623
Emaz Hassan <64681129+Emaz1999@users.noreply.github.com>
2020-05-03 14:21:59 +0500

    Player.cpp is changed by Emaz (2)

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,21 @@
+#include "Player.h"
+
+bool Player::NoSix()
+{
+	return true;
+	//Written just so that error can be avoided
+}
+bool Player::hasNoInitialisedPiece()
+{
+	return true;
+	//Written just so that error can be avoided
+}
+void Player::CheckTurn()
+{
+
+}
+void Player::move()
+{
+
+}
 
