5d8149659aeab50ed1a8f0e20bbeb56bdfb8c372
MuhammadIbrahim0 <64721208+MuhammadIbrahim0@users.noreply.github.com>
2020-05-03 13:43:26 +0500

    Update ScoreBoard.h

--- ScoreBoard.h
+++ ScoreBoard.h
@@ -1,11 +1,17 @@
 #pragma once
 class ScoreBoard
 {
+private:
+	int* scores;
+	int nos;
+public:
 	ScoreBoard() {
 
 	}
 	~ScoreBoard() {
 
 	}
+	void displayScores(int* n);
+	void selectScore(int sc);  
 };
 
