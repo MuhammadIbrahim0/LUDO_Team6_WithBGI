6862e01aeefb01bd6cf18c68bb39070828023c86
mmuneeburahman <mmuneeburahman@gmail.com>
2020-05-03 14:37:38 +0500

    updated Enums
    Added PIECECOLOR

--- Enums.h
+++ Enums.h
@@ -1,1 +1,9 @@
 #pragma once
+enum class PIECECOLOR {
+	RED = 0,
+	YELLOW = 1,
+	GREEN = 2,
+	BLUE = 3,
+	GRAY = 4,
+	PURPLE = 5
+};
--- Ludu.h
+++ Ludu.h
@@ -1,15 +1,25 @@
 #pragma once
+#include "Enums.h"
+#include "board.h"
+#include "Dice.h"
+#include "Player.h"
+#include "ScoreBoard.h"
+#include "Position.h"
 #include <iostream>
 using std::cout;
 using std::endl;
 class Ludu
 {
+
 public:
 	Ludu() {
 		cout << "ludu Contructed" << endl;
 	}
+
+
 	~Ludu() {
 		cout << "ludu Destructed" << endl;
 	}
+
 };
 
