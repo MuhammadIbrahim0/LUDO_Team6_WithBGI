d8fb6ca9fab92fc58db8b062b8fbff1743dc509a
MuhammadIbrahim0 <64721208+MuhammadIbrahim0@users.noreply.github.com>
2020-05-10 00:13:23 +0500

    Update board.cpp

---
+++ board.cpp
@@ -0,0 +1,34 @@
+#include"board.h"
+
+void Board::move(int selectedPosition, int selectedScores) 
+{
+	// Player class me changes is in my team 
+	// Box, Piece
+}
+
+bool Board::hasKilled(int pos) 
+{
+
+}
+
+void Board::askToUndraw(int pos) 
+{
+
+}
+
+void Board::askToDraw(int pos)
+{
+
+}
+
+void Board::removeFromTheBoard(int pos) 
+{
+
+}
+
+/*
+void putstops();
+void putspawn();
+void isstop();
+bool isstop(int pos);
+*/
