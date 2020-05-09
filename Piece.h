bcb9fbbeeba749f69fa17ce885f5b59ad6883dcb
MuhammadIbrahim0 <64721208+MuhammadIbrahim0@users.noreply.github.com>
2020-05-09 20:58:46 +0500

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -13,7 +13,13 @@ private:
 	bool Iskill = false;
 	bool succeeded;
 	bool IsSix;
+	LUDOCOLORS m_color;
+	friend class Box;
 public:
+	LUDOCOLORS getColor()
+	{
+		return m_color;
+	}
 	void draw();
 	void move();
 	bool Mypiece();
