58c935e0d0d3d859ee139730bc8c3a4a85ccf63b
mmuneeburahman <56772228+mmuneeburahman@users.noreply.github.com>
2020-05-10 15:48:01 +0500

    Update Piece

--- Piece.h
+++ Piece.h
@@ -4,19 +4,20 @@
 class Piece
 {
 private:
-	Position P;
+	int Pos;
 	int Radius;
 	int NoofSteps;
 	bool RoundCompleted = false;
 	bool Iskill = false;
 	bool Succeeded;
 	bool IsSix;
-	LUDOCOLORS m_color;
+	LUDOCOLORS Color;
 public:
 	Piece() {}
+	Piece(int radius, int n);
 	LUDOCOLORS getColor()
 	{
-		return m_color;
+		return Color;
 	}
 	bool isInLobby()
 	{
@@ -31,11 +32,17 @@ public:
 		return true;
 	}
 
-	void draw() {
-
+	void draw(Position P) {
+		setcolor(static_cast<int>(Color));
+		circle(P.getX(), P.getY(), Radius);
+		setfillstyle(SOLID_FILL, static_cast<int>(Color));
+		floodfill(P.getX(), P.getY(), static_cast<int>(Color));
 	}
-	void unDraw(LUDOCOLORS color) {
-
+	void unDraw(LUDOCOLORS color, Position P) {
+		setcolor(static_cast<int>(color));
+		circle(P.getX(), P.getY(), Radius);
+		setfillstyle(SOLID_FILL, static_cast<int>(color));
+		floodfill(P.getX(), P.getY(), static_cast<int>(color));
 	}
 	void move(Position S, Position E) {
 
